5e864fd101f2affe7989504b79d9b5eba281a6bd
DESKTOP-OQ0R5HA\Adnan Bashir <dev.haseeb22@gmail.com>
2023-08-20 09:12:56 +0500

    add effect, skip selection panel and scripts readable

--- Bowling_Defense/Assets/Haseeb/Scripts/Ball.cs
+++ Bowling_Defense/Assets/Haseeb/Scripts/Ball.cs
@@ -8,30 +8,21 @@ public class Ball : MonoBehaviour
     [SerializeField] private float Speed;
     [SerializeField] bool isgamelose;
     public bool GameLose() => isgamelose;
-    [SerializeField] Vector3 BallDestroyPosition;
-    [SerializeField] Vector3 BallPosition;
     private void Start()
     {
 
         rb = GetComponent<Rigidbody>();
     }
-
-    // Update is called once per frame
     private void Update()
     {
-        BallPosition = transform.position;
-        rb.AddForce(Vector3.forward * Speed * Time.deltaTime);
-        if (transform.position.z >= BallDestroyPosition.z)
-        {
-            isgamelose = true;
-        }
+        rb.AddForce(Vector3.forward * Speed * Time.deltaTime);   
     }
     private void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.CompareTag("Pin"))
         {
-            Debug.Log("Collision Enter");
             isgamelose = true;
+            Debug.Log("Collision Enter");
         }
     }
 }
